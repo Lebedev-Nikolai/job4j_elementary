2afefd4a8733d081fdd0d066152ed7bb1ed87e0d
Lebedev-Nikolai <Lebedev_Nikolai@mail.ru>
2024-10-07 18:39:09 +0300

    Перевернуть массив [#505231 #515667] v2

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int lastIndex = array.length - 1;
-
-        for (int currentIndex = 0; currentIndex < lastIndex; currentIndex++) {
+        for (int currentIndex = 0; currentIndex < array.length / 2; currentIndex++) {
             int temp = array[currentIndex];
-            array[currentIndex] = array[lastIndex];
-            array[lastIndex] = temp;
-            lastIndex--;
+
+            array[currentIndex] = array[array.length - currentIndex - 1];
+            array[array.length - currentIndex - 1] = temp;
         }
 
         return array;
